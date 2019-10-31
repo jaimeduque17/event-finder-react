93ed30910290ba2bafd62fee8f8f69b98daacedf
Jaime Duque <jaimeduque17@gmail.com>
2019-10-31 00:20:17 -0500

    All the components are implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
+import EventsList from './components/EventsList';
 
 import ProviderCategories from './context/ContextCategories';
+import ProviderEvents from './context/ContextEvents';
 
 function App() {
   return (
-    <ProviderCategories>
-      <Header />
-    </ProviderCategories>
+    <ProviderEvents>
+      <ProviderCategories>
+        <Header />
+        <div className="uk-container">
+          <Form />
+          <EventsList />
+        </div>
+      </ProviderCategories>
+    </ProviderEvents>
   );
 }
 
